70e543ac97ca5aa8855965733d68e12d1c28aacb
bobwildfong <bob@seeds.ca>
2018-01-29 17:40:20 -0500

    therapist options

--- index.php
+++ index.php
@@ -5,10 +5,12 @@ include( SEEDCore."SEEDCore.php" );
 
 $screen = SEEDInput_Str( 'screen' );
 
-switch( $screen ) {
-    case 'admin':     $s = drawAdmin();      break;
-    case 'therapist': $s = drawTherapist();  break;
-    default:          $s = drawHome();       break;
+if( $screen == 'admin' ) {
+    $s = drawAdmin();
+} else if( substr( $screen, 0, 9 ) == "therapist" ) {
+    $s = drawTherapist( $screen );
+} else {
+    $s = drawHome();
 }
 
 echo $s;
@@ -25,13 +27,63 @@ function drawHome()
     return( $s );
 }
 
-function drawTherapist()
+function drawTherapist( $screen )
 {
-    $s = "";
-
     $s = "<h2>Therapist</h2>";
 
-    $s .= "<a href='?screen=home'><button>Home</button></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href='?screen=admin'><button>Admin</button></a>";
+    switch( $screen ) {
+        case "therapist":
+        default:
+            $s .= "<p>What would you like to do?</p>"
+                 ."<a href='?screen=home'><button>Home</button></a>"
+                 ."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+                 ."<a href='?screen=therapist-materials'><button>Print Materials</button></a>"
+                 ."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+                 ."<a href='?screen=therapist-formscharts'><button>Print Forms for Charts</button></a>"
+                 ."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+                 ."<a href='?screen=therapist-linedpapers'><button>Print Different Lined Papers</button></a>"
+                 ."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+                 ."<a href='?screen=therapist-entercharts'><button>Enter Charts</button></a>"
+                 ."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+                 ."<a href='?screen=therapist-clientRx'><button>Print Client Rx Activities</button></a>"
+                 ."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+                 ."<a href='?screen=therapist-ideas'><button>Get Ideas</button></a>"
+                 ."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+                 ."<a href='?screen=therapist-downloadcustommaterials'><button>Download and Customize Marketable Materials</button></a>"
+                 ."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+                 ."<a href='?screen=therapist-team'><button>Meet the Team</button></a>"
+                 ."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+                 ."<a href='?screen=therapist-submitresources'><button>Submit Resources to Share</button></a>"
+                 ;
+            break;
+        case "therapist-materials":
+            $s .= "PRINT MATERIALS";
+            break;
+        case "therapist-formscharts":
+            $s .= "PRINT FORMS FOR CHARTS";
+            break;
+        case "therapist-linedpapers":
+            $s .= "PRINT DIFFERENT LINED PAPERS";
+            break;
+        case "therapist-entercharts":
+            $s .= "ENTER CHARTS";
+            break;
+        case "therapist-clientRx":
+            $s .= "PRINT CLIENT Rx ACTIVITIES";
+            break;
+        case "therapist-ideas":
+            $s .= "GET IDEAS";
+            break;
+        case "therapist-downloadcustommaterials":
+            $s .= "DOWNLOAD AND CUSTOMIZE MARKETABLE MATERIALS";
+            break;
+        case "therapist-team":
+            $s .= "MEET THE TEAM";
+            break;
+        case "therapist-submitresources":
+            $s .= "SUBMIT RESOURCES";
+            break;
+    }
 
     return( $s );
 }
