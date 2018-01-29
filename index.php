bdccf3e9fb624e46d37cd78b228519b73359ec78
bobwildfong <bob@seeds.ca>
2018-01-29 14:49:05 -0500

    start!

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
-include( "../seedcore/SEEDCore.php" );
+include_once( "_config.php" );
+include( SEEDCore."SEEDCore.php" );
 
 $screen = SEEDInput_Str( 'screen' );
 
@@ -15,24 +16,34 @@ echo $s;
 function drawHome()
 {
     $s = "";
-    
-    
+
+
+    $s = "<h2>Home</h2>";
+
+    $s .= "<a href='?screen=therapist'><button>Therapist</button></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href='?screen=admin'><button>Admin</button></a>";
+
     return( $s );
 }
 
 function drawTherapist()
 {
     $s = "";
-    
-    
+
+    $s = "<h2>Therapist</h2>";
+
+    $s .= "<a href='?screen=home'><button>Home</button></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href='?screen=admin'><button>Admin</button></a>";
+
     return( $s );
 }
 
 function drawAdmin()
 {
     $s = "";
-    
-    
+
+    $s = "<h2>Admin</h2>";
+
+    $s .= "<a href='?screen=home'><button>Home</button></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href='?screen=therapist'><button>Therapist</button></a>";
+
     return( $s );
 }
 
