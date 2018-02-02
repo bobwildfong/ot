b04ad939b5d4a9ac1cb9cde215f516085566913f
bobwildfong <bob@seeds.ca>
2018-02-01 20:10:03 -0500

    bootstrap

--- index.php
+++ index.php
@@ -3,17 +3,45 @@
 include_once( "_config.php" );
 include( SEEDCore."SEEDCore.php" );
 
+if( !isset($dirBootstrap) ) {
+    $dirBootstrap = "./bootstrap3/";
+}
+
+$s =
+"<html>
+<head>
+<link rel='stylesheet' type='text/css' href='".$dirBootstrap."dist/css/bootstrap.min.css'></link>
+<script src='".$dirBootstrap."dist/js/bootstrap.min.js'></script>
+<meta name='viewport' content='width=device-width, initial-scale=1.0'>
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+<script src='//oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js'></script>
+<script src='//oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js'></script>
+<![endif]-->
+
+<style>
+.otButton { border:1px solid #aaa; width:80%; text-align:center; padding: 20px 0px;}
+</style>
+</head>
+<body>";
+
+
+
+
 $screen = SEEDInput_Str( 'screen' );
 
 if( $screen == 'admin' ) {
-    $s = drawAdmin();
+    $s .= drawAdmin();
 } else if( substr( $screen, 0, 9 ) == "therapist" ) {
-    $s = drawTherapist( $screen );
+    $s .= drawTherapist( $screen );
 } else {
-    $s = drawHome();
+    $s .= drawHome();
 }
 
-echo $s;
+echo $s
+    ."</body></html>";
+
 
 function drawHome()
 {
@@ -35,26 +63,44 @@ function drawTherapist( $screen )
         case "therapist":
         default:
             $s .= "<p>What would you like to do?</p>"
-                 ."<a href='?screen=home'><button>Home</button></a>"
-                 ."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
-                 ."<a href='?screen=therapist-materials'><button>Print Materials</button></a>"
-                 ."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
-                 ."<a href='?screen=therapist-formscharts'><button>Print Forms for Charts</button></a>"
-                 ."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
-                 ."<a href='?screen=therapist-linedpapers'><button>Print Different Lined Papers</button></a>"
-                 ."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
-                 ."<a href='?screen=therapist-entercharts'><button>Enter Charts</button></a>"
-                 ."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
-                 ."<a href='?screen=therapist-clientRx'><button>Print Client Rx Activities</button></a>"
-                 ."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
-                 ."<a href='?screen=therapist-ideas'><button>Get Ideas</button></a>"
-                 ."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
-                 ."<a href='?screen=therapist-downloadcustommaterials'><button>Download and Customize Marketable Materials</button></a>"
-                 ."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
-                 ."<a href='?screen=therapist-team'><button>Meet the Team</button></a>"
-                 ."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
-                 ."<a href='?screen=therapist-submitresources'><button>Submit Resources to Share</button></a>"
-                 ;
+                 ."<div class='container-fluid'>"
+                     ."<div class='row'>"
+                         ."<div class='col-md-3'>"
+                             ."<a href='?screen=home'><div class='otButton'>Home</div></a>"
+                         ."</div>"
+                         ."<div class='col-md-3'>"
+                             ."<a href='?screen=therapist-materials'><div class='otButton'>Print Materials</div></a>"
+                         ."</div>"
+                         ."<div class='col-md-3'>"
+                             ."<a href='?screen=therapist-formscharts'><button>Print Forms for Charts</button></a>"
+                         ."</div>"
+                         ."<div class='col-md-3'>"
+                             ."<a href='?screen=therapist-linedpapers'><button>Print Different Lined Papers</button></a>"
+                         ."</div>"
+                     ."</div>"
+                     ."<div class='row'>"
+                         ."<div class='col-md-3'>"
+                             ."<a href='?screen=therapist-entercharts'><button>Enter Charts</button></a>"
+                         ."</div>"
+                         ."<div class='col-md-3'>"
+                             ."<a href='?screen=therapist-clientRx'><button>Print Client Rx Activities</button></a>"
+                         ."</div>"
+                         ."<div class='col-md-3'>"
+                             ."<a href='?screen=therapist-ideas'><button>Get Ideas</button></a>"
+                         ."</div>"
+                         ."<div class='col-md-3'>"
+                             ."<a href='?screen=therapist-downloadcustommaterials'><button>Download and Customize Marketable Materials</button></a>"
+                         ."</div>"
+                     ."</div>"
+                     ."<div class='row'>"
+                         ."<div class='col-md-3'>"
+                             ."<a href='?screen=therapist-team'><button>Meet the Team</button></a>"
+                         ."</div>"
+                         ."<div class='col-md-3'>"
+                             ."<a href='?screen=therapist-submitresources'><button>Submit Resources to Share</button></a>"
+                         ."</div>"
+                     ."</div>"
+                 ."</div>";
             break;
         case "therapist-materials":
             $s .= "PRINT MATERIALS";
