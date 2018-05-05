0e123c06a0e954ebc9c1b7d16aafdd2a411e94f3
bobwildfong <bob@seeds.ca>
2018-05-05 13:18:46 -0400

    new jx.php

---
+++ jx.php
@@ -0,0 +1,48 @@
+<?php
+
+/* Entry point for CATS AJAX
+ *
+ * Input:
+ *     cmd = the command to execute
+ *
+ * Output:
+ *     bOk   = true/false; the command was successful
+ *     sOut  = text, string, html output for a successful command
+ *     raOut = json-ized array of output values for a successful command
+ *     sErr  = error message if !bOk
+ */
+
+require_once "_config.php" ;
+require_once "database.php" ;
+
+//header( "Content-type: application/json" );
+
+$rJX = array( 'bOk' => false,
+              'sOut' => "",
+              'raOut' => array(),
+              'sErr' => "",
+);
+
+$cmd = SEEDInput_Str('cmd');
+
+/* Establish secure session here - this is an open entry point where anyone can fake an ajax request with their own url.
+ * Just like the main index.php we have to be sure that the request is coming from an authorized user.
+ * Fortunately, this ajax request should be coming from the same browser that's running the main CATS program so we will have
+ * the same PHP_SESSION here. That means we can just use the same authentication method as index.php
+ */
+
+
+switch( $cmd ) {
+    case 'appt-newform':
+        $rJX['sOut'] = "Hello World";
+        $rJX['bOk'] = true;
+        break;
+}
+
+
+
+done:
+
+echo json_encode($rJX);
+
+?>
